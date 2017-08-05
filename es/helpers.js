89eaabf9d150ae847c5dea96bc6777cd28338aed
Vinicius Dacal <viniciusldacal@gmail.com>
2017-08-05 13:41:51 -0300

    remove built files from git

--- es/helpers.js
+++
@@ -1,6 +0,0 @@
-export var isString = function isString(str) {
-  return typeof str === 'string';
-};
-export var checkAction = function checkAction(type) {
-  return type.length === 2 && type.every(isString);
-};
--- es/index.js
+++
@@ -1,6 +0,0 @@
-import asyncActionHelpers from './asyncActionHelpers';
-import policies from './policies';
-import { createApiActions } from './apiActionCreator';
-import { createAsyncMiddleware } from './middleware';
-
-export { asyncActionHelpers, policies, createApiActions, createAsyncMiddleware };
